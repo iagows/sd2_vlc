fba354d1392ae6e5a2a717842cb96bca5cf80987
iagows <el.iagows@gmail.com>
2015-02-02 17:21:44 -0300

    Code organization

--- main.cpp
+++ main.cpp
@@ -121,12 +121,14 @@ SDL_Renderer * createRenderer()
     return ren;
 }
 
-void stayOnLoop(Context* c)
+void playLoop(Context* c, libvlc_media_player_t *mp)
 {
     SDL_Event event;
     bool done = false;
     int action = 0;
     bool pause = false;
+
+    bool previousPause = false;
 
     // Main loop.
     while(!done)
@@ -157,29 +159,53 @@ void stayOnLoop(Context* c)
                 break;
         }
 
-        if(!pause) { c->n++; }
+        if(!pause)
+        {
+            c->n++;
+        }
+
+        if(!previousPause && pause)
+        {
+            libvlc_media_player_pause(mp);
+            previousPause = true;
+        }
+        else if(previousPause && !pause)
+        {
+            libvlc_media_player_play(mp);
+            previousPause = false;
+        }
 
         SDL_Delay(1000/10);
     }
 }
 
+void createPlayerAndPlay(libvlc_instance_t* libvlc, std::string path, Context* context)
+{
+    libvlc_media_t *m;
+    libvlc_media_player_t *mp;
+
+    m = libvlc_media_new_path(libvlc, path.c_str());
+    mp = libvlc_media_player_new_from_media(m);
+    libvlc_media_release(m);
+
+    libvlc_video_set_callbacks(mp, lock, unlock, display, context);
+    libvlc_video_set_format(mp, "RV16", VIDEOWIDTH, VIDEOHEIGHT, VIDEOWIDTH*2);
+    libvlc_media_player_play(mp);
+
+    playLoop(context, mp);
+
+    // Stop stream and clean up libVLC.
+    libvlc_media_player_stop(mp);
+    libvlc_media_player_release(mp);
+    libvlc_release(libvlc);
+
+    // Close window and clean up libSDL.
+    //delete context;//it isn't a pointer, so it'll clean itself
+}
+
 int main()
 {
     std::string path = "a.mp4";
-    libvlc_instance_t *libvlc;
-    libvlc_media_t *m;
-    libvlc_media_player_t *mp;
-    char const *vlc_argv[] = {
-
-        //"--no-audio"//, // Don't play audio.
-        //"--no-xlib"//, // Don't use Xlib.
-
-        // Apply a video filter.
-        //"--video-filter", "sepia", "--sepia-intensity=200"
-    };
-    int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
-
-    Context context;
 
     // Initialise libSDL.
     if(SDL_Init(SDL_INIT_VIDEO) < 0)
@@ -188,6 +214,7 @@ int main()
         return EXIT_FAILURE;
     }
 
+    Context context;
     context.renderer = createRenderer();
 
     context.texture = SDL_CreateTexture(
@@ -202,13 +229,21 @@ int main()
 
     context.mutex = SDL_CreateMutex();
 
-    // If you don't have this variable set you must have plugins directory
-    // with the executable or libvlc_new() will not work!
-#ifdef DEBUG
-    printf("VLC_PLUGIN_PATH=%s\n", getenv("VLC_PLUGIN_PATH"));
-#endif
+    // If you don't have this variable set you must have plugins directory with the executable or libvlc_new() will not work!
+    //printf("VLC_PLUGIN_PATH=%s\n", getenv("VLC_PLUGIN_PATH"));
 
     // Initialise libVLC.
+    libvlc_instance_t *libvlc;
+    char const *vlc_argv[] = {
+
+        //"--no-audio", // Don't play audio.
+        //"--no-xlib", // Don't use Xlib.
+
+        // Apply a video filter.
+        //"--video-filter", "sepia", "--sepia-intensity=200"
+    };
+    int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
+
     libvlc = libvlc_new(vlc_argc, vlc_argv);
     if(libvlc == nullptr)
     {
@@ -216,25 +251,9 @@ int main()
         return EXIT_FAILURE;
     }
 
-    m = libvlc_media_new_path(libvlc, path.c_str());//m = libvlc_media_new_path(libvlc, argv[1]);
-    mp = libvlc_media_player_new_from_media(m);
-    libvlc_media_release(m);
+    createPlayerAndPlay(libvlc, path, &context);
 
-    libvlc_video_set_callbacks(mp, lock, unlock, display, &context);
-    libvlc_video_set_format(mp, "RV16", VIDEOWIDTH, VIDEOHEIGHT, VIDEOWIDTH*2);
-    libvlc_media_player_play(mp);
+    quit(EXIT_SUCCESS);
 
-    stayOnLoop(&context);
-
-    // Stop stream and clean up libVLC.
-    libvlc_media_player_stop(mp);
-    libvlc_media_player_release(mp);
-    libvlc_release(libvlc);
-
-    // Close window and clean up libSDL.
-    //delete context;//it isn't a pointer, so it'll clean itself
-
-    quit(0);
-
-    return 0;
+    return EXIT_SUCCESS;
 }
